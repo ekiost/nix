527508d3dfe251948c9f644c6070ebce1642dcb3
ekiost <choonkeatling@icloud.com>
2026-07-23 01:47:13 +0800

    Configure Plasma dark theme, empty session, and icon taskbar
    - Apply BreezeDark color scheme and look-and-feel
    - Set login mode to empty session
    - Switch to icon tasks with Dolphin, Firefox, Steam, Kitty, and System Settings launchers

--- modules/home/plasma.nix
+++ modules/home/plasma.nix
@@ -2,16 +2,24 @@
 {
   programs.plasma = {
     enable = true;
+    workspace = {
+      colorScheme = "BreezeDark";
+      lookAndFeel = "org.kde.breezedark.desktop";
+    };
+    configFile."ksmserverrc".General.loginMode = "emptySession";
     panels = [
       {
         location = "bottom";
         widgets = [
           "org.kde.plasma.kickoff"
           {
-            name = "org.kde.plasma.taskmanager";
+            name = "org.kde.plasma.icontasks";
             config.General.launchers = [
-              "applications:kitty.desktop"
+              "applications:org.kde.dolphin.desktop"
+              "applications:firefox.desktop"
               "applications:steam.desktop"
+              "applications:kitty.desktop"
+              "applications:systemsettings.desktop"
             ];
           }
           "org.kde.plasma.marginsseparator"
